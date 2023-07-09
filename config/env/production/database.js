df4c300038e859b5b7163d971c203662876d8f06
Clément Dumas <clement.dumas38@gmail.com>
2023-07-09 19:12:04 +0200

    setup deployement env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,16 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    connection: {
-      client: 'postgres',
+    module.exports = ({ env }) => ({
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
       },
-      debug: false,
-    },
-  }
-};
+    });
